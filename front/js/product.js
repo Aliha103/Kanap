7c4d3b60438a0f45015a2203a6690b4005b80e75
Ali Hassan Cheema <acheema152@yahoo.com>
2023-08-07 00:39:51 +0500

    retriving the data from localstorage if there is or make it null

--- front/js/product.js
+++ front/js/product.js
@@ -1,2 +1,15 @@
 //Retrieve the "id" query parameter from current URL
 const kanapPageId = new URLSearchParams(window.location.search).get("id");
+
+// Selecting the DOM in which work perform
+const {
+  item__img: shelfImgKanap,
+  title: nomKanap,
+  price: prixKanap,
+  description: speechKanap,
+  colors: colorOptions,
+  quantity: getProductQuantity,
+} = document.querySelector;
+
+// from localstorage retriving the data
+const getBasket = () => JSON.parse(localStorage.getItem("kanapLS")) || [];
